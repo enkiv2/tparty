0b35e2d299d1174f91ec552c16c3cdf69aae8ebb
enkiv2 <john.ohno@gmail.com>
2014-12-01 08:31:11 -0500

    remove dos line feeds; actually translate to japanese (google translate uses ja for japanese instead of jp); aggressively strip backslashes and change quoting; route around argument size limitations aggressively rather than dropping long lines

--- tparty.sh
+++ tparty.sh
@@ -9,22 +9,22 @@ function rlang() {
 
 LC_ALL=c
 fromlang='en'
-tolang='jp'
+tolang='ja'
 maxiter=23	# If we don't have agreement between two iterations by this point, just keep our last iteration
-while read line ; do
+tr -d '\r' | fmt | while read line ; do
 	tolang=$(rlang)
 	i=maxiter
-	old=$line
-	new="$(termit $tolang $fromlang "$(termit $fromlang $tolang "$old" | sed 's/^=> //')" | sed 's/^=> //')"
+	old=$line 
+	new="$(termit $tolang $fromlang "$(termit $fromlang $tolang "$old" | sed 's/^=> //' )" | sed 's/^=> //')"
 	while [[ "$old" != "$new" ]] ; do
-		if [[ $i -lt 1 ]] ; then 
+		if ( [[ $i -lt 1 ]] || [[ $(echo $new | wc -c) -gt 1023 ]] ) ; then 
 			old=$new
 		else
 			i=$((i-1))
 			old=$new
-			new="$(termit $tolang $fromlang "$(termit $fromlang $tolang "$old" | sed 's/^=> //')" | sed 's/^=> //')"
+			new="$(termit $tolang $fromlang "$(termit $fromlang $tolang "$old" | sed 's/^=> //'  )" | sed 's/^=> //;s/\\\\*//g;' )"
 		fi
 	done
-	echo "$new"
-done
+	echo "$new" | sed 's/\\\\*//g'
+done 
 
